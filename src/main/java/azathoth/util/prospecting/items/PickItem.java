d84150235d9c460996c4c4956c6810e45aeb405c
Hannah <hannahks@protonmail.com>
2021-12-23 10:51:09 -0800

    Pick works with any block in the stone OreDict

--- src/main/java/azathoth/util/prospecting/items/PickItem.java
+++ src/main/java/azathoth/util/prospecting/items/PickItem.java
@@ -5,6 +5,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class PickItem extends ProspectingItem {
 	protected PickItem() {
@@ -13,7 +14,7 @@ public class PickItem extends ProspectingItem {
 
 	@Override
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitx, float hity, float hitz) {
-		if (world.getBlock(x, y, z).equals(Blocks.stone)) {
+		if (OreDictionary.getOres("stone").contains(new ItemStack(world.getBlock(x, y, z)))) {
 			super.doProspect(player, world, x, y, z);
 			return true;
 		}
